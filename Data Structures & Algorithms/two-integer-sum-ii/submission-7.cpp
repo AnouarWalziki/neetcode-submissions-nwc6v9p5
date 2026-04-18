6898fd7366cb4740ef6f51da618646c41ac433e9
AnouarWalziki <82971155+AnouarWalziki@users.noreply.github.com>
2026-04-18 13:15:14 +0200

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res; 
+        for(int i = 0; i < numbers.size(); i++){
+            if(i != 0 && numbers[i] == numbers[i-1]){
+                continue;
+            }
+
+            int lookup = target - numbers[i];
+            int left = i + 1;
+            int right = numbers.size() - 1;
+            cout << "left "<< left << endl;
+            while(left != right){
+                cout << "left "<< left << endl;
+                cout << "right "<< right << endl;
+                int midle = (right + left) / 2;
+                cout << "midle "<< midle << endl;
+                if(numbers[midle] == lookup){
+                    left = right = midle;
+                    break;
+                }
+                else if(lookup > numbers[midle]){
+                    left = midle + 1; 
+                } else {
+                    right = midle - 1;
+                }
+            }
+            if(numbers[left] == lookup){
+                res.insert(res.end(), {i+1, left + 1});
+                break;
+            }
+        }
+        return res;
+    }
+};
