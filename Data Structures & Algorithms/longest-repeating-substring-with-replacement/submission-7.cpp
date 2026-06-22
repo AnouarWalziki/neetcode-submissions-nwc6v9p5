f18cef2262d2cdbc335e9f436256e34e0b2c69aa
AnouarWalziki <82971155+AnouarWalziki@users.noreply.github.com>
2026-06-22 20:36:24 +0200

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0;
+        int c = k;
+        int res = 0;
+
+        for(; r < s.size(); r++){
+            if(s[r] != s[l]){
+                if(c > 0)
+                    c--;
+                else{
+                    res = max(res, r - l);
+                    c = k;
+                    l = r - k;
+                    r = l;
+                }
+            }
+        }
+
+        if(l > c)
+            return max(res, r - l + c);
+        return s.size();
+    }
+};
